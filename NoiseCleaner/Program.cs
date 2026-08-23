49b187c0f8b1239a3c33fb872e7a4d5a54050988
강우석 / Kang wooseok <60039295+banggae2@users.noreply.github.com>
2026-08-23 18:55:39 +0900

    Add application entry point

---
+++ NoiseCleaner/Program.cs
@@ -0,0 +1,11 @@
+namespace NoiseCleaner;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
